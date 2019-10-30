36f907405e474be6ba8a8b2f0edd6fcef6bc3045
cruigo <cruigo93@gmail.com>
2019-10-30 23:06:19 +0600

    bpmi.sh added

---
+++ bpmi.sh
@@ -0,0 +1,11 @@
+CORPUS=text8
+
+scripts/clean_corpus.sh $CORPUS > $CORPUS.clean
+
+mkdir w5.sub
+python hyperwords/corpus2pairs.py  ${CORPUS}.clean > w5.sub/pairs
+scripts/pairs2counts.sh w5.sub/pairs > w5.sub/counts
+python hyperwords/counts2vocab.py w5.sub/counts
+
+python hyperwords/counts2pmi.py --t 1 w5.sub/counts w5.sub/pmi
+
